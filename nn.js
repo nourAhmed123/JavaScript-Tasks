78a816b762d10cf280ea8e7d5a168fd36ba7b95e
nourAhmed123 <96887154+nourAhmed123@users.noreply.github.com>
2022-07-20 13:32:01 +0200

    first commit

--- nn.js
+++ nn.js
@@ -1,17 +1,11 @@
+var response = prompt("Enter Your Message");
+var i = 1;
+while (i < 7) {
+  // document.write("<h"+i+">" ,response);
+  //first commit
+  const para = document.createElement("h" + i);
+  //ttgrgrgr;
+  document.body.appendChild(para); //3shan a-append fel body mehtaga a3mel el script tag fel body mesh fo2
 
-var response=prompt("Enter Your Message");
-var i=1;
-while(i<7){
-
-    // document.write("<h"+i+">" ,response);
-  
-    const para = document.createElement("h"+i);
-
-    para.innerText =response;  
-    
-     document.body.appendChild(para); //3shan a-append fel body mehtaga a3mel el script tag fel body mesh fo2 
-  
- 
-    i++;
+  i++;
 }
-
