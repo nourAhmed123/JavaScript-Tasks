a6e2ccf2bb46451a1011884bb0d1dd37570d905f
nourAhmed123 <96887154+nourAhmed123@users.noreply.github.com>
2022-07-20 13:38:43 +0200

    second

--- nn.js
+++ nn.js
@@ -6,6 +6,6 @@ while (i < 7) {
   const para = document.createElement("h" + i);
   //ttgrgrgr;
   document.body.appendChild(para); //3shan a-append fel body mehtaga a3mel el script tag fel body mesh fo2
-
+  //thrthtrhrh
   i++;
 }
